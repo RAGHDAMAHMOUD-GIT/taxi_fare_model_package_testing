89a1cc5f79f68391eaa2911b17e686328559645a
Raghda Tarek <raghda.tarek.hussein@gmail.com>
2022-02-23 00:54:20 +0100

    developed the old file

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
+
 setup(name='TaxiFareModel',
       version="1.0",
       description="Project Description",
