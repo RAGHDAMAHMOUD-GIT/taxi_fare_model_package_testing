fb68aa06c84665db3ecbcd265fcdbdd8d7a5e93b
Raghda Tarek <raghda.tarek.hussein@gmail.com>
2022-02-23 00:38:36 +0100

    developed the old file

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,10 +1,20 @@
+import joblib
+from termcolor import colored
+import mlflow
 from TaxiFareModel.data import get_data, clean_data
 from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
 from TaxiFareModel.utils import compute_rmse
+from memoized_property import memoized_property
+from mlflow.tracking import MlflowClient
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+EXPERIMENT_NAME = "first_experiment"
 
 
 class Trainer():
@@ -16,6 +26,11 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = EXPERIMENT_NAME
+
+    def set_experiment_name(self, experiment_name):
+        '''defines the experiment name for MLFlow'''
+        self.experiment_name = experiment_name
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -53,17 +68,46 @@ class Trainer():
         rmse = compute_rmse(y_pred, y_test)
         return round(rmse, 2)
 
+    def save_model(self):
+        """Save the model into a .joblib format"""
+        joblib.dump(self.pipeline, 'model.joblib')
+        print(colored("model.joblib saved locally", "green"))
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
 
 if __name__ == "__main__":
-    N = 10_000
+    # Get and clean data
+    N = 10000
     df = get_data(nrows=N)
     df = clean_data(df)
     y = df["fare_amount"]
     X = df.drop("fare_amount", axis=1)
-    from sklearn.model_selection import train_test_split
-
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-    trainer = Trainer(X_train, y_train)
+    # Train and save model, locally and
+    trainer = Trainer(X=X_train, y=y_train)
+    trainer.set_experiment_name('xp2')
     trainer.run()
     rmse = trainer.evaluate(X_test, y_test)
     print(f"rmse: {rmse}")
+    trainer.save_model()
